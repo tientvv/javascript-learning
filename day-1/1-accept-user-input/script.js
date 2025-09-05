4b78b7999b381a442c61cf285a3da377e588ecb0
tientvv <tientranvan19052004@gmail.com>
2025-09-05 16:51:44 +0700

    deleted

--- day-1/1-accept-user-input/script.js
+++
@@ -1,7 +0,0 @@
-let username;
-
-document.getElementById('mySubmit').onclick = function () {
-  username = document.getElementById('myText').value;
-  console.log(username);
-  document.getElementById('myH1').textContent = `Hello ${username}`;
-};
